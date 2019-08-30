459a17017598a926055cbd3a3514363f3b2bc3dd
hieptq-0855 <tran.quang.hiep@sun-asterisk.com>
2019-08-30 08:32:24 +0700

    Create models

---
+++ src/Model/Table/AccountsTable.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Model\Table;
+
+use Cake\ORM\Query;
+use Cake\ORM\RulesChecker;
+use Cake\ORM\Table;
+use Cake\Validation\Validator;
+
+/**
+ * Accounts Model
+ *
+ * @property \App\Model\Table\UsersTable&\Cake\ORM\Association\HasMany $Users
+ *
+ * @method \App\Model\Entity\Account get($primaryKey, $options = [])
+ * @method \App\Model\Entity\Account newEntity($data = null, array $options = [])
+ * @method \App\Model\Entity\Account[] newEntities(array $data, array $options = [])
+ * @method \App\Model\Entity\Account|false save(\Cake\Datasource\EntityInterface $entity, $options = [])
+ * @method \App\Model\Entity\Account saveOrFail(\Cake\Datasource\EntityInterface $entity, $options = [])
+ * @method \App\Model\Entity\Account patchEntity(\Cake\Datasource\EntityInterface $entity, array $data, array $options = [])
+ * @method \App\Model\Entity\Account[] patchEntities($entities, array $data, array $options = [])
+ * @method \App\Model\Entity\Account findOrCreate($search, callable $callback = null, $options = [])
+ */
+class AccountsTable extends Table
+{
+    /**
+     * Initialize method
+     *
+     * @param array $config The configuration for the Table.
+     * @return void
+     */
+    public function initialize(array $config)
+    {
+        parent::initialize($config);
+
+        $this->setTable('accounts');
+        $this->setDisplayField('id');
+        $this->setPrimaryKey('id');
+
+        $this->hasMany('Users', [
+            'foreignKey' => 'account_id',
+        ]);
+    }
+
+    /**
+     * Default validation rules.
+     *
+     * @param \Cake\Validation\Validator $validator Validator instance.
+     * @return \Cake\Validation\Validator
+     */
+    public function validationDefault(Validator $validator)
+    {
+        $validator
+            ->integer('id')
+            ->allowEmptyString('id', null, 'create');
+
+        $validator
+            ->scalar('user_name')
+            ->maxLength('user_name', 255)
+            ->requirePresence('user_name', 'create')
+            ->notEmptyString('user_name');
+
+        $validator
+            ->scalar('password')
+            ->maxLength('password', 255)
+            ->requirePresence('password', 'create')
+            ->notEmptyString('password');
+
+        $validator
+            ->integer('role')
+            ->requirePresence('role', 'create')
+            ->notEmptyString('role');
+
+        return $validator;
+    }
+}
